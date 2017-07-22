2885a31eccc1d78e21a22634c65b679459a835a6
xiaocui723 <xiaocui723@163.com>
2017-07-22 19:16:04 +0800

    添加 User 入口

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,9 @@
  * Time: 18:57
  */
 
-class User
-{
+class User extends CI_Controller {
 
+    public function login() {
+        return json_encode(array('user_id' => '123456', 'user_name' => 'bicycle'));
+    }
 }
